0b2b3807259cb1bb3deb9e4213a0bb76c2587d94
Miguel Angel Solano Diaz <msolano684@unab.edu.co>
2026-02-19 12:09:06 -0500

    Navbar-footer universal

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="footer">
+    <p>0 2026 JetProduct Inc. -Built for Developers</p>
+</footer>
---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,10 @@
+<nav>
+    <a href="#" class="navbar-brand">JetProduct</a>
+    <ul class="nav-links">
+        <li><a href="#">Dashboard</a></li>
+        <li><a href="#"></a>Inventario</li>
+        <li><a href="#"></a>Ventas</li>
+        <li><a href="#"></a>Configuracion</li>
+
+    </ul>
+</nav>
