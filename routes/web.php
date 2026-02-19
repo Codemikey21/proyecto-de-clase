99f944fa181fdf7be176bf488f4067dcc0661ba9
Miguel Angel Solano Diaz <msolano684@unab.edu.co>
2026-02-19 11:07:42 -0500

    Grupo de rutas

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
 
-Route::get('/product', [ProductController::class, 'index']);
+Route::prefix('product')->controller(ProductController::class) ->group(function(){
+    Route::get('/','index');
+    Route::get('/create',  'create');
+    Route::get('/{producto}','show');
+});
 
-Route::get('/product/create', [ProductController::class, 'create']);
-
-Route::get('/product/{producto}', [ProductController::class, 'show']);
